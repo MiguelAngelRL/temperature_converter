1bc627323b92a30a5bc6c83978894a54b2e0696e
miguelangelrl <12910511+MiguelAngelRL@users.noreply.github.com>
2021-03-12 21:35:33 +0100

    Basic text input fields added

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Temperature conversion tool
-        </p>
-      </header>
+      <p>
+        Temperature conversion tool
+      </p>
+      <input type="number" id="cel" step="0.1"/>
+      <input type="number" id="fahr" step="0.1"/>
     </div>
   );
 }
